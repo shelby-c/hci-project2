5a0915687af7e5a878c6c9607cf80f6d25f23a15
shelby <coe.shelby@gmail.com>
2022-12-05 20:00:59 -0500

    template

---
+++ shelby.js
@@ -0,0 +1,552 @@
+const data = [
+    {
+      "stateName": "Alabama",
+      "avgTeacherStartingSalary": 41028,
+      "avgSATScore": 1143,
+      "avgACTScore": 18.9,
+      "NAEPScoreReading": 263,
+      "NAEPScoreMath": 273,
+      "highschoolGradRate": 92,
+      "percentCompletingCollege": 25.5,
+      "eduSpendingPerPupil": 10116.28614
+    },
+    {
+      "stateName": "Alaska",
+      "avgTeacherStartingSalary": 48469,
+      "avgSATScore": 1097,
+      "avgACTScore": 20.1,
+      "NAEPScoreReading": 270,
+      "NAEPScoreMath": 301,
+      "highschoolGradRate": 80,
+      "percentCompletingCollege": 29.6,
+      "eduSpendingPerPupil": 18313.42176
+    },
+    {
+      "stateName": "Arizona",
+      "avgTeacherStartingSalary": 39057,
+      "avgSATScore": 1134,
+      "avgACTScore": 19,
+      "NAEPScoreReading": 272,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 78,
+      "percentCompletingCollege": 29.5,
+      "eduSpendingPerPupil": 8766.402019
+    },
+    {
+      "stateName": "Arkansas",
+      "avgTeacherStartingSalary": 35201,
+      "avgSATScore": 1141,
+      "avgACTScore": 19.3,
+      "NAEPScoreReading": 261,
+      "NAEPScoreMath": 278,
+      "highschoolGradRate": 88,
+      "percentCompletingCollege": 23,
+      "eduSpendingPerPupil": 10344.90046
+    },
+    {
+      "stateName": "California",
+      "avgTeacherStartingSalary": 49303,
+      "avgSATScore": 1065,
+      "avgACTScore": 22.6,
+      "NAEPScoreReading": 262,
+      "NAEPScoreMath": 288,
+      "highschoolGradRate": 85,
+      "percentCompletingCollege": 33.9,
+      "eduSpendingPerPupil": 14053.17631
+    },
+    {
+      "stateName": "Colorado",
+      "avgTeacherStartingSalary": 35292,
+      "avgSATScore": 1024,
+      "avgACTScore": 23.8,
+      "NAEPScoreReading": 272,
+      "NAEPScoreMath": 297,
+      "highschoolGradRate": 81,
+      "percentCompletingCollege": 40.9,
+      "eduSpendingPerPupil": 11539.5305
+    },
+    {
+      "stateName": "Connecticut",
+      "avgTeacherStartingSalary": 46905,
+      "avgSATScore": 1046,
+      "avgACTScore": 25.5,
+      "NAEPScoreReading": 267,
+      "NAEPScoreMath": 293,
+      "highschoolGradRate": 89,
+      "percentCompletingCollege": 39.3,
+      "eduSpendingPerPupil": 21345.6021
+    },
+    {
+      "stateName": "Delaware",
+      "avgTeacherStartingSalary": 43092,
+      "avgSATScore": 985,
+      "avgACTScore": 24,
+      "NAEPScoreReading": "'NULL'",
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 89,
+      "percentCompletingCollege": 32,
+      "eduSpendingPerPupil": 14756.56734
+    },
+    {
+      "stateName": "Florida",
+      "avgTeacherStartingSalary": 38724,
+      "avgSATScore": 999,
+      "avgACTScore": 20.1,
+      "NAEPScoreReading": 260,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 87,
+      "percentCompletingCollege": 29.9,
+      "eduSpendingPerPupil": 9936.722694
+    },
+    {
+      "stateName": "Georgia",
+      "avgTeacherStartingSalary": 38509,
+      "avgSATScore": 1058,
+      "avgACTScore": 21.4,
+      "NAEPScoreReading": "'NULL'",
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 82,
+      "percentCompletingCollege": 31.3,
+      "eduSpendingPerPupil": "'NULL'"
+    },/*
+    {
+      "stateName": "Hawaii",
+      "avgTeacherStartingSalary": 48428,
+      "avgSATScore": 1100,
+      "avgACTScore": 19,
+      "NAEPScoreReading": 259,
+      "NAEPScoreMath": 287,
+      "highschoolGradRate": 85,
+      "percentCompletingCollege": 33,
+      "eduSpendingPerPupil": "'NULL'"
+    },*/
+    {
+      "stateName": "Idaho",
+      "avgTeacherStartingSalary": 38015,
+      "avgSATScore": 993,
+      "avgACTScore": 22.5,
+      "NAEPScoreReading": 267,
+      "NAEPScoreMath": 294,
+      "highschoolGradRate": 81,
+      "percentCompletingCollege": 27.6,
+      "eduSpendingPerPupil": 8271.566953
+    },
+    {
+      "stateName": "Illinois",
+      "avgTeacherStartingSalary": 40484,
+      "avgSATScore": 1013,
+      "avgACTScore": 24.3,
+      "NAEPScoreReading": 278,
+      "NAEPScoreMath": 300,
+      "highschoolGradRate": 86,
+      "percentCompletingCollege": 34.7,
+      "eduSpendingPerPupil": 17340.02105
+    },/*
+    {
+      "stateName": "Indiana",
+      "avgTeacherStartingSalary": 37573,
+      "avgSATScore": 1080,
+      "avgACTScore": 22.5,
+      "NAEPScoreReading": 269,
+      "NAEPScoreMath": 298,
+      "highschoolGradRate": 87,
+      "percentCompletingCollege": 26.5,
+      "eduSpendingPerPupil": "'NULL'"
+    },*/
+    {
+      "stateName": "Iowa",
+      "avgTeacherStartingSalary": 37908,
+      "avgSATScore": 1244,
+      "avgACTScore": 21.6,
+      "NAEPScoreReading": 248,
+      "NAEPScoreMath": 263,
+      "highschoolGradRate": 92,
+      "percentCompletingCollege": 28.6,
+      "eduSpendingPerPupil": 11958.12875
+    },
+    {
+      "stateName": "Kansas",
+      "avgTeacherStartingSalary": 38314,
+      "avgSATScore": 1241,
+      "avgACTScore": 21.2,
+      "NAEPScoreReading": 288,
+      "NAEPScoreMath": 307,
+      "highschoolGradRate": 87,
+      "percentCompletingCollege": 33.4,
+      "eduSpendingPerPupil": 12696.75921
+    },
+    {
+      "stateName": "Kentucky",
+      "avgTeacherStartingSalary": 37238,
+      "avgSATScore": 1232,
+      "avgACTScore": 19.8,
+      "NAEPScoreReading": 254,
+      "NAEPScoreMath": 282,
+      "highschoolGradRate": 91,
+      "percentCompletingCollege": 24.2,
+      "eduSpendingPerPupil": 11396.83529
+    },/*
+    {
+      "stateName": "Louisiana",
+      "avgTeacherStartingSalary": 41747,
+      "avgSATScore": 1200,
+      "avgACTScore": 18.8,
+      "NAEPScoreReading": 262,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 80,
+      "percentCompletingCollege": 24.1,
+      "eduSpendingPerPupil": "'NULL'"
+    },
+    {
+      "stateName": "Maine",
+      "avgTeacherStartingSalary": 36380,
+      "avgSATScore": 1013,
+      "avgACTScore": 24.3,
+      "NAEPScoreReading": 261,
+      "NAEPScoreMath": 296,
+      "highschoolGradRate": 87,
+      "percentCompletingCollege": 31.8,
+      "eduSpendingPerPupil": "'NULL'"
+    },*/
+    {
+      "stateName": "Maryland",
+      "avgTeacherStartingSalary": 47959,
+      "avgSATScore": 1058,
+      "avgACTScore": 22.3,
+      "NAEPScoreReading": 271,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 87,
+      "percentCompletingCollege": 40.2,
+      "eduSpendingPerPupil": 14643.81636
+    },
+    {
+      "stateName": "Massachusetts",
+      "avgTeacherStartingSalary": 47396,
+      "avgSATScore": 1120,
+      "avgACTScore": 25.5,
+      "NAEPScoreReading": 273,
+      "NAEPScoreMath": 298,
+      "highschoolGradRate": 88,
+      "percentCompletingCollege": 43.7,
+      "eduSpendingPerPupil": 18268.60485
+    },
+    {
+      "stateName": "Michigan",
+      "avgTeacherStartingSalary": 37549,
+      "avgSATScore": 1003,
+      "avgACTScore": 24.4,
+      "NAEPScoreReading": 254,
+      "NAEPScoreMath": 287,
+      "highschoolGradRate": 81,
+      "percentCompletingCollege": 29.1,
+      "eduSpendingPerPupil": 13071.80858
+    },
+    {
+      "stateName": "Minnesota",
+      "avgTeacherStartingSalary": 40310,
+      "avgSATScore": 1284,
+      "avgACTScore": 21.4,
+      "NAEPScoreReading": 261,
+      "NAEPScoreMath": 289,
+      "highschoolGradRate": 84,
+      "percentCompletingCollege": 36.1,
+      "eduSpendingPerPupil": 13603.18324
+    },
+    {
+      "stateName": "Mississippi",
+      "avgTeacherStartingSalary": 36543,
+      "avgSATScore": 1237,
+      "avgACTScore": 18.4,
+      "NAEPScoreReading": 272,
+      "NAEPScoreMath": 280,
+      "highschoolGradRate": 85,
+      "percentCompletingCollege": 22,
+      "eduSpendingPerPupil": 9653.974656
+    },
+    {
+      "stateName": "Missouri",
+      "avgTeacherStartingSalary": 32970,
+      "avgSATScore": 1236,
+      "avgACTScore": 20.8,
+      "NAEPScoreReading": 269,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 90,
+      "percentCompletingCollege": 29.2,
+      "eduSpendingPerPupil": 11249.37626
+    },
+    {
+      "stateName": "Montana",
+      "avgTeacherStartingSalary": 32871,
+      "avgSATScore": 1199,
+      "avgACTScore": 19.8,
+      "NAEPScoreReading": 270,
+      "NAEPScoreMath": 298,
+      "highschoolGradRate": 87,
+      "percentCompletingCollege": 32,
+      "eduSpendingPerPupil": 12105.35386
+    },
+    {
+      "stateName": "Nebraska",
+      "avgTeacherStartingSalary": 35820,
+      "avgSATScore": 1260,
+      "avgACTScore": 20,
+      "NAEPScoreReading": 266,
+      "NAEPScoreMath": 289,
+      "highschoolGradRate": 88,
+      "percentCompletingCollege": 31.9,
+      "eduSpendingPerPupil": 13004.147
+    },/*
+    {
+      "stateName": "Nevada",
+      "avgTeacherStartingSalary": 40732,
+      "avgSATScore": 1156,
+      "avgACTScore": 17.9,
+      "NAEPScoreReading": 264,
+      "NAEPScoreMath": 294,
+      "highschoolGradRate": 84,
+      "percentCompletingCollege": 24.7,
+      "eduSpendingPerPupil": "'NULL'"
+    },*/
+    {
+      "stateName": "New Hampshire",
+      "avgTeacherStartingSalary": 38990,
+      "avgSATScore": 1059,
+      "avgACTScore": 25,
+      "NAEPScoreReading": "'NULL'",
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 88,
+      "percentCompletingCollege": 37,
+      "eduSpendingPerPupil": 18952.14615
+    },
+    {
+      "stateName": "New Jersey",
+      "avgTeacherStartingSalary": 53177,
+      "avgSATScore": 1090,
+      "avgACTScore": 24.2,
+      "NAEPScoreReading": 261,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 91,
+      "percentCompletingCollege": 39.7,
+      "eduSpendingPerPupil": 20629.6443
+    },/*
+    {
+      "stateName": "New Mexico",
+      "avgTeacherStartingSalary": 41214,
+      "avgSATScore": 1073,
+      "avgACTScore": 19.3,
+      "NAEPScoreReading": 273,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 75,
+      "percentCompletingCollege": 27.3,
+      "eduSpendingPerPupil": "'NULL'"
+    },
+    {
+      "stateName": "New York",
+      "avgTeacherStartingSalary": 47181,
+      "avgSATScore": 1064,
+      "avgACTScore": 24.5,
+      "NAEPScoreReading": 268,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 83,
+      "percentCompletingCollege": 36.6,
+      "eduSpendingPerPupil": 25520.36024
+    },*/
+    {
+      "stateName": "North Carolina",
+      "avgTeacherStartingSalary": 37049,
+      "avgSATScore": 1100,
+      "avgACTScore": 19,
+      "NAEPScoreReading": 271,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 87,
+      "percentCompletingCollege": 31.3,
+      "eduSpendingPerPupil": 9957.985473
+    },/*
+    {
+      "stateName": "North Dakota",
+      "avgTeacherStartingSalary": 40106,
+      "avgSATScore": 1263,
+      "avgACTScore": 19.9,
+      "NAEPScoreReading": 267,
+      "NAEPScoreMath": 291,
+      "highschoolGradRate": 88,
+      "percentCompletingCollege": 30,
+      "eduSpendingPerPupil": "'NULL'"
+    },*/
+    {
+      "stateName": "Ohio",
+      "avgTeacherStartingSalary": 37569,
+      "avgSATScore": 1097,
+      "avgACTScore": 20,
+      "NAEPScoreReading": 261,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 82,
+      "percentCompletingCollege": 28.3,
+      "eduSpendingPerPupil": 13804.54123
+    },
+    {
+      "stateName": "Oklahoma",
+      "avgTeacherStartingSalary": 37992,
+      "avgSATScore": 963,
+      "avgACTScore": 18.9,
+      "NAEPScoreReading": 276,
+      "NAEPScoreMath": 302,
+      "highschoolGradRate": 85,
+      "percentCompletingCollege": 25.5,
+      "eduSpendingPerPupil": 9512.041651
+    }, /*
+    {
+      "stateName": "Oregon",
+      "avgTeacherStartingSalary": 38280,
+      "avgSATScore": 1112,
+      "avgACTScore": 21.1,
+      "NAEPScoreReading": 263,
+      "NAEPScoreMath": 291,
+      "highschoolGradRate": 80,
+      "percentCompletingCollege": 33.7,
+      "eduSpendingPerPupil": "'NULL'"
+    },*/
+    {
+      "stateName": "Pennsylvania",
+      "avgTeacherStartingSalary": 46232,
+      "avgSATScore": 1082,
+      "avgACTScore": 23.6,
+      "NAEPScoreReading": 260,
+      "NAEPScoreMath": 306,
+      "highschoolGradRate": 87,
+      "percentCompletingCollege": 31.4,
+      "eduSpendingPerPupil": 17141.64616
+    },
+    {
+      "stateName": "Rhode Island",
+      "avgTeacherStartingSalary": 43569,
+      "avgSATScore": 995,
+      "avgACTScore": 24.7,
+      "NAEPScoreReading": "'NULL'",
+      "NAEPScoreMath": 259,
+      "highschoolGradRate": 84,
+      "percentCompletingCollege": 34.2,
+      "eduSpendingPerPupil": 17024.14253
+    },/*
+    {
+      "stateName": "South Carolina",
+      "avgTeacherStartingSalary": 37550,
+      "avgSATScore": 1030,
+      "avgACTScore": 18.8,
+      "NAEPScoreReading": 280,
+      "NAEPScoreMath": 305,
+      "highschoolGradRate": 81,
+      "percentCompletingCollege": 28.1,
+      "eduSpendingPerPupil": "'NULL'"
+    },*/
+    {
+      "stateName": "South Dakota",
+      "avgTeacherStartingSalary": 39636,
+      "avgSATScore": 1268,
+      "avgACTScore": 21.6,
+      "NAEPScoreReading": 266,
+      "NAEPScoreMath": 289,
+      "highschoolGradRate": 84,
+      "percentCompletingCollege": 28.8,
+      "eduSpendingPerPupil": 10233.78315
+    },/*
+    {
+      "stateName": "Tennessee",
+      "avgTeacherStartingSalary": 38809,
+      "avgSATScore": 1220,
+      "avgACTScore": 19.4,
+      "NAEPScoreReading": 265,
+      "NAEPScoreMath": 296,
+      "highschoolGradRate": 91,
+      "percentCompletingCollege": 27.3,
+      "eduSpendingPerPupil": "'NULL'"
+    },
+    {
+      "stateName": "Texas",
+      "avgTeacherStartingSalary": 44582,
+      "avgSATScore": 1022,
+      "avgACTScore": 20.5,
+      "NAEPScoreReading": 285,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 90,
+      "percentCompletingCollege": 29.9,
+      "eduSpendingPerPupil": "'NULL'"
+    },
+    {
+      "stateName": "Utah",
+      "avgTeacherStartingSalary": 43026,
+      "avgSATScore": 1230,
+      "avgACTScore": 20.3,
+      "NAEPScoreReading": "'NULL'",
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 87,
+      "percentCompletingCollege": 34,
+      "eduSpendingPerPupil": "'NULL'"
+    },*/
+    {
+      "stateName": "Vermont",
+      "avgTeacherStartingSalary": 40241,
+      "avgSATScore": 1106,
+      "avgACTScore": 24.1,
+      "NAEPScoreReading": 277,
+      "NAEPScoreMath": 297,
+      "highschoolGradRate": 85,
+      "percentCompletingCollege": 38,
+      "eduSpendingPerPupil": 21313.99081
+    },
+    {
+      "stateName": "Virginia",
+      "avgTeacherStartingSalary": 42069,
+      "avgSATScore": 1119,
+      "avgACTScore": 24,
+      "NAEPScoreReading": 270,
+      "NAEPScoreMath": 297,
+      "highschoolGradRate": 88,
+      "percentCompletingCollege": 38.8,
+      "eduSpendingPerPupil": 12904.51852
+    },
+    {
+      "stateName": "Washington",
+      "avgTeacherStartingSalary": 49113,
+      "avgSATScore": 1074,
+      "avgACTScore": 22.1,
+      "NAEPScoreReading": 236,
+      "NAEPScoreMath": "'NULL'",
+      "highschoolGradRate": 81,
+      "percentCompletingCollege": 36,
+      "eduSpendingPerPupil": 14555.71182
+    },/*
+    {
+      "stateName": "West Virginia",
+      "avgTeacherStartingSalary": 37978,
+      "avgSATScore": 943,
+      "avgACTScore": 20.8,
+      "NAEPScoreReading": 260,
+      "NAEPScoreMath": 291,
+      "highschoolGradRate": 91,
+      "percentCompletingCollege": 20.6,
+      "eduSpendingPerPupil": "'NULL'"
+    },
+    {
+      "stateName": "Wisconsin",
+      "avgTeacherStartingSalary": 38678,
+      "avgSATScore": 1283,
+      "avgACTScore": 20.3,
+      "NAEPScoreReading": "'NULL'",
+      "NAEPScoreMath": 285,
+      "highschoolGradRate": 90,
+      "percentCompletingCollege": 30.1,
+      "eduSpendingPerPupil": "'NULL'"
+    },*/
+    {
+      "stateName": "Wyoming",
+      "avgTeacherStartingSalary": 46558,
+      "avgSATScore": 1238,
+      "avgACTScore": 19.8,
+      "NAEPScoreReading": 282,
+      "NAEPScoreMath": 303,
+      "highschoolGradRate": 82,
+      "percentCompletingCollege": 27.4,
+      "eduSpendingPerPupil": 16698.20871
+    }
+  ]
